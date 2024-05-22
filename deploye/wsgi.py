d3017207b77cf57e6099cae0b81d7e5487d62fbf
ABHISHEKCHARATE <abhicharate02@gmail.com>
2024-05-22 11:30:04 +0530

    changed in setting.py , wsgi.py

--- deploye/wsgi.py
+++ deploye/wsgi.py
@@ -7,11 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deploye.settings')
+
+# application = get_wsgi_application()
+# app = application
+
+
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deploye.settings')
 
 application = get_wsgi_application()
-app = application
